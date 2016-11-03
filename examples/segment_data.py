1c091731363b6072c7973e3a03f1caaa6784b8dc
Dani Mora de Checa <daniel.mora.checa@gmail.com>
2016-11-03 16:27:12 +0100

    Fixed segmentation data example

--- examples/segment_data.py
+++ examples/segment_data.py
@@ -1,14 +1,16 @@
 # !/usr/bin/python
 
+import os
 from neuralimg.image import preproc as pr
+from neuralimg.crag.merge_mc import *
 
 """ Generates superpixels given the membrane probabilities """
 
 mask = 7
 sigma = 0.25
-gt_folder = 'data/crag/gt'
 sps_folder = 'data/crag/sps'
 mems_folder = 'data/crag/mem'
+mems_norm = 'data/crag/mem_norm'
 
 # Extract images
 proc = pr.DatasetProc(mems_folder)
@@ -16,6 +18,12 @@ proc.read()
 proc.segment(mask, sigma)
 proc.save_data(sps_folder)
 
+# Generate normalized membranes [0,1]
+proc = pr.DatasetProc(mems_folder)
+proc.read()
+proc.normalize()
+proc.save_data(mems_norm)
+
 # MC Extractor
 mc_path = os.path.join(sps_folder, 'histories_mc')
-mt.MCTreeExtractor(sps_folder, mems_folder).extract(mc_path)
+MCTreeExtractor(sps_folder, mems_norm).extract(mc_path)
