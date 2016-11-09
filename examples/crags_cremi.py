2f6bcc7fe1a93601c2e0c23cc7541157edec0393
Dani Mora de Checa <daniel.mora.checa@gmail.com>
2016-11-09 09:43:07 +0100

    Updated CRAG sample A parameters

--- examples/crags_cremi.py
+++ examples/crags_cremi.py
@@ -32,12 +32,13 @@ if __name__ == '__main__':
 
     # Parameters per sample
     # These are not the only optimal parameters. We have several combinations of 
-    # them having similar merge and split errors
-    paramsA = {'name': 'A', 'mask': 5, 'sigma': 0.25, 'thresh': 142.82, 'haus': 500 } 
+    # them having similar merge and split errors. Maximum Hausdorff distances to 
+    # be tested
+    paramsA = {'name': 'A', 'mask': 5, 'sigma': 0.25, 'thresh': 10.5, 'haus': 500 } 
     paramsB = {'name': 'B', 'mask': 9, 'sigma': 0.5, 'thresh': 25.5, 'haus': 500 } 
     paramsC = {'name': 'C', 'mask': 7, 'sigma': 1, 'thresh': 41.0, 'haus': 500 }
 
-    # Paths per sample
+    # Paths per sample: Modify them pointing to your folders
     # Recommended to use processed version for the groundtruth
     # pr.join_small()
     # that erases noisy regions (specially in sample A)
